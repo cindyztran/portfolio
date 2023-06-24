a99572c2d12bd56e3428449e26ebcbb38d31f1b1
Cindy Tran <cindyztran@gmail.com>
2023-06-24 14:06:32 -0700

    update: add image, update styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,43 @@
+/* eslint-disable react/no-unescaped-entities */
+import Image from "next/image";
+import laptop from "./assets/images/laptop.png";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col justify-between p-24">
-      <div>
-        <h5 className="text-7xl py-2">Cindy Tran</h5>
-        <h4 className="text-4xl">Full Stack Developer</h4>
+    <main className="flex justify-between p-24">
+      <div
+        className="text-4xl"
+        style={{ fontFamily: "Menlo, Monaco, Courier New, monospace" }}
+      >
+        <span style={{ color: "#4688CC" }}>const</span>
+        <span className="px-2" style={{ color: "#42A7FE" }}>
+          cindyTran
+        </span>
+        =
+        <span className="ps-2" style={{ color: "#FFD00B" }}>
+          &#123;
+        </span>
+        <div className="ps-10">
+          <span style={{ color: "#7DBADD" }}>role:</span>
+          <span className="ps-2" style={{ color: "#C07D65" }}>
+            "Full Stack Developer"
+          </span>
+          ,
+        </div>
+        <div className="ps-10">
+          <span style={{ color: "#7DBADD" }}>location:</span>
+          <span className="ps-2" style={{ color: "#C07D65" }}>
+            "Seattle"
+          </span>
+          ,
+        </div>
+        <span style={{ color: "#FFD00B" }}>&#125;</span>;
       </div>
+      <Image
+        src={laptop}
+        width={500}
+        height={500}
+        alt="Picture of the author"
+      />
     </main>
   );
 }
